5360ee1207a8cc4211efe6a81b5d905d09039ce2
yousefahmed98 <36170310+yousefahmed98@users.noreply.github.com>
2020-02-25 23:05:44 +0200

    Revert "Update Main.java"
    This reverts commit 0dc28f7cbacce69e410511cdf467c9a746bcf2da.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
-			//test
 			new ReallySimpleSubscriber(),
 	};
 	public static void main(String[] args) {
