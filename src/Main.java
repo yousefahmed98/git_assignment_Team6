bfda917d4e07860fde124ce2f2c413db31fbea51
yousefahmed98 <36170310+yousefahmed98@users.noreply.github.com>
2020-02-25 23:03:21 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
+			//test
 			new ReallySimpleSubscriber(),
 	};
 	public static void main(String[] args) {
