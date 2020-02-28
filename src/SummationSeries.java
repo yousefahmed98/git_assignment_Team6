725a9827a15c07841b1827f477a7f14e06872a4c
heba mohamed <36227122+hebaamohaamed@users.noreply.github.com>
2020-02-28 21:21:18 +0200

    Update SummationSeries.java
    20170329

--- src/SummationSeries.java
+++ src/SummationSeries.java
@@ -1,18 +1,13 @@
-public class SummationSeries {
-private int number;
-    public SummationSeries(int n)
-    {
-        number=n;
-    }
-
-    public int sum()
-    {
-        //return ((number*(number+1))/2);
-        int sum=0;
+public class SummationSeries implements ISubscriber {
+    @Override
+    public void notifySubscriber(String input) {
+        int number = Integer.parseInt(input);
+        System.out.println("Summation Series = "+((number*(number+1))/2));
+        /*int sum=0;
         for (int i=1;i<=number;i++)
         {
             sum+=i;
         }
-        return sum;
+        System.out.println("Summation Series = "+ sum);*/
     }
 }
