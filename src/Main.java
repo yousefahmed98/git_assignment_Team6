6c73790e54e1ab824479c07be78ef002a12366fd
yousefahmed98 <36170310+yousefahmed98@users.noreply.github.com>
2020-03-01 21:07:03 +0200

    remove ununnecessary classes ReallySimpleSubscriber and SimpleSubscriber

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Main {
 
 	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
-			new ReallySimpleSubscriber(),
 			new MultiplicationSeries(),
-			new LucasSeries()
-			new SphereArea () ,
-		  new SphereVolume(),
+			new LucasSeries(),
+			new SphereArea (),
+			new SphereVolume(),
 			new circleVolume(),
-      new SphereCircumference()
-
+      		new SphereCircumference()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
