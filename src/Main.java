a126d92fe531648e19fd7fd4c305061af57d10f3
kareem Magdy <kareemmagdy1@yahoo.com>
2020-03-01 20:03:41 +0200

    Update Main.java
    20170197
    
    This is Test Main for SphereCircumference

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Main {
 
-	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
-			//test
-			new ReallySimpleSubscriber(),
+	private static ISubscriber subscribers [] = {new SphereCircumference()
+			,new SphereCircumference()
+			,new SphereCircumference()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
 		for (ISubscriber sub : subscribers) {
 			mathTopic.addSubscriber(sub);
 		}
-		Scanner sc = new Scanner(System.in);
-		String input = sc.next();
-		mathTopic.dispatchEvent(input);
+//		Scanner sc = new Scanner(System.in);
+//		String input = sc.next();
+		mathTopic.dispatchEvent("5");
 	}
 }
