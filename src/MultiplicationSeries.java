9805b0f977aaafd5d0e51bed598b96c6ccea64e8
yousefahmed98 <36170310+yousefahmed98@users.noreply.github.com>
2020-03-01 20:35:45 +0200

    MultiplicationSeries is done

--- src/MultiplicationSeries.java
+++ src/MultiplicationSeries.java
@@ -2,7 +2,12 @@ public class MultiplicationSeries implements ISubscriber {
 
     @java.lang.Override
     public void notifySubscriber(String input) {
-
+        int num = Integer.parseInt(input);
+        int fact=1;
+        for(int i=1;i<=num;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+num+" is: "+fact);
     }
 
 }
