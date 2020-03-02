4eb9112d6bbcfe0bba612fb0d0e5e942a7454283
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2020-03-03 01:00:53 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 			new LucasSeries(),
 			new SphereArea (),
 			new SphereVolume(),
-			new circleVolume(),
+			new ballVolume(),
 		  new SphereCircumference(),
 			new CircleCircumference(),
 			new CircleArea(),
@@ -16,13 +16,106 @@ public class Main {
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
-		for (ISubscriber sub : subscribers) {
-			mathTopic.addSubscriber(sub);
-		}
-		
+		System.out.println("What do you prefer\n 1-Do all the operations at once\n 2-Choose only one of them");
 		Scanner sc = new Scanner(System.in);
-		String input = sc.next();
-		mathTopic.dispatchEvent(input);
+		int choice = Integer.parseInt(sc.nextLine());
+		
+		if (choice == 1) {
+			for (ISubscriber sub : subscribers) {
+				mathTopic.addSubscriber(sub);
+			}
+			
+			String input = sc.next();
+			mathTopic.dispatchEvent(input);
+		}
+		else if (choice == 2) {
+			System.out.println("Choose one\n "
+					+ "1-SummationSeries\n "
+					+ "2-LucasSeries\n "
+					+ "3-CircleCircumference\n "
+					+ "4-SphereVolume\n "
+					+ "5-BallVolume\n "
+					+ "6-MultiplicationSeries\n "
+					+ "7-SphereArea\n "
+					+ "8-Fibonacci\n "
+					+ "9-CircleArea\n "
+					+ "0-SphereCircumference");
+			int toDo = Integer.parseInt(sc.next());
+			if (toDo == 1) {
+				ISubscriber temp = new SummationSeries();
+				mathTopic.addSubscriber(temp);
+				System.out.print("number of terms = ");
+				String n = sc.next();
+				mathTopic.dispatchEvent(n);
+			}
+			else if (toDo == 2) {
+				ISubscriber temp = new LucasSeries();
+				mathTopic.addSubscriber(temp);
+				System.out.print("number = ");
+				String n = sc.next();
+				mathTopic.dispatchEvent(n);
+			}
+			else if (toDo == 3) {
+				ISubscriber temp = new CircleCircumference();
+				mathTopic.addSubscriber(temp);
+				System.out.print("redius of the circle = ");
+				String r = sc.next();
+				mathTopic.dispatchEvent(r);
+			}
+			else if (toDo == 4) {
+				ISubscriber temp = new SphereVolume();
+				mathTopic.addSubscriber(temp);
+				System.out.print("redius of the sphere = ");
+				String r = sc.next();
+				mathTopic.dispatchEvent(r);
+			}
+			else if (toDo == 5) {
+				ISubscriber temp = new ballVolume();
+				mathTopic.addSubscriber(temp);
+				System.out.print("redius of the ball = ");
+				String r = sc.next();
+				mathTopic.dispatchEvent(r);
+			}
+			else if (toDo == 6) {
+				ISubscriber temp = new MultiplicationSeries();
+				mathTopic.addSubscriber(temp);
+				System.out.print("number = ");
+				String n = sc.next();
+				mathTopic.dispatchEvent(n);
+			}
+			else if (toDo == 7) {
+				ISubscriber temp = new SphereArea();
+				mathTopic.addSubscriber(temp);
+				System.out.print("redius of the sphere = ");
+				String r = sc.next();
+				mathTopic.dispatchEvent(r);
+			}
+			else if (toDo == 8) {
+				ISubscriber temp = new Fibonacci();
+				mathTopic.addSubscriber(temp);
+				System.out.print("number of terms = ");
+				String n = sc.next();
+				mathTopic.dispatchEvent(n);
+			}
+			else if (toDo == 9) {
+				ISubscriber temp = new CircleArea();
+				mathTopic.addSubscriber(temp);
+				System.out.print("redius of the circle = ");
+				String r = sc.next();
+				mathTopic.dispatchEvent(r);
+			}
+			else if (toDo == 0) {
+				ISubscriber temp = new SphereCircumference();
+				mathTopic.addSubscriber(temp);
+				System.out.print("redius of the sphere = ");
+				String r = sc.next();
+				mathTopic.dispatchEvent(r);
+			}
+			else 
+				System.out.println("invalid choice");
+		}
+		else 
+			System.out.println("invalid choice");
 		sc.close();
 	}
 }
